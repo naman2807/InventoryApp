1a9e82287ece27a7f8e50185503414e91dc54c27
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-02 10:35:24 +0530

    Specify the Item as the only class with the list of entities

--- app/src/main/java/com/example/inventory/data/ItemRoomDatabase.kt
+++ app/src/main/java/com/example/inventory/data/ItemRoomDatabase.kt
@@ -3,6 +3,6 @@ package com.example.inventory.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database
+@Database(entities = [Item::class], version = 1, exportSchema = false)
 abstract class ItemRoomDatabase: RoomDatabase() {
 }
