5fb0e37b6c3a5fea02194fef3c553237815dee82
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-02 11:02:42 +0530

    Implement the create() method. Check if the modelClass is the same as the InventoryViewModel class and return an instance of it. Otherwise, throw an exception.

--- app/src/main/java/com/example/inventory/data/InventoryViewModel.kt
+++ app/src/main/java/com/example/inventory/data/InventoryViewModel.kt
@@ -8,7 +8,11 @@ class InventoryViewModel(private val itemDao: ItemDao):ViewModel() {
 
 class InventoryViewModelFactory(private val itemDao: ItemDao): ViewModelProvider.Factory{
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        TODO("Not yet implemented")
+        if (modelClass.isAssignableFrom(InventoryViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return InventoryViewModel(itemDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 
 }
