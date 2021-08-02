7c8da2347727d01e286f3d89613f8726991735ac
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-02 10:42:06 +0530

    To create the database instance, call .build()

--- app/src/main/java/com/example/inventory/data/ItemRoomDatabase.kt
+++ app/src/main/java/com/example/inventory/data/ItemRoomDatabase.kt
@@ -22,6 +22,7 @@ abstract class ItemRoomDatabase: RoomDatabase() {
                     "item_database"
                 )
                     .fallbackToDestructiveMigration()
+                    .build()
             }
         }
     }
