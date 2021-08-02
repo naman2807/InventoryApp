a144ea90fda804dc6f8199ec15a690ae45b82118
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-02 10:44:19 +0530

    create a val called database of the type ItemRoomDatabase.

--- app/src/main/java/com/example/inventory/InventoryApplication.kt
+++ app/src/main/java/com/example/inventory/InventoryApplication.kt
@@ -16,5 +16,10 @@
 package com.example.inventory
 
 import android.app.Application
+import com.example.inventory.data.ItemRoomDatabase
 
-class InventoryApplication : Application()
+class InventoryApplication : Application(){
+    val database : ItemRoomDatabase by lazy {
+        ItemRoomDatabase.getDatabase(this)
+    }
+}
