052c6cc6f24c208b98d91d91295cfde6f1502f30
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-02 10:39:51 +0530

    Inside the synchronized block, create a val instance variable, and use the database builder to get the database

--- app/src/main/java/com/example/inventory/data/ItemRoomDatabase.kt
+++ app/src/main/java/com/example/inventory/data/ItemRoomDatabase.kt
@@ -2,6 +2,7 @@ package com.example.inventory.data
 
 import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Item::class], version = 1, exportSchema = false)
@@ -15,7 +16,7 @@ abstract class ItemRoomDatabase: RoomDatabase() {
 
         fun getDatabase(context: Context):ItemRoomDatabase{
             return INSTANCE ?: synchronized(this){
-
+                val instance = Room.databaseBuilder()
             }
         }
     }
