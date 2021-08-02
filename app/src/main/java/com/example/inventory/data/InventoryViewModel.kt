a242c0a28c60b7d3b3b806711ae6213396188596
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-02 11:02:01 +0530

    At the end of the InventoryViewModel.kt file outside the class, add InventoryViewModelFactory class to instantiate the InventoryViewModel instance.

--- app/src/main/java/com/example/inventory/data/InventoryViewModel.kt
+++ app/src/main/java/com/example/inventory/data/InventoryViewModel.kt
@@ -1,6 +1,14 @@
 package com.example.inventory.data
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
 class InventoryViewModel(private val itemDao: ItemDao):ViewModel() {
+}
+
+class InventoryViewModelFactory(private val itemDao: ItemDao): ViewModelProvider.Factory{
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        TODO("Not yet implemented")
+    }
+
 }
