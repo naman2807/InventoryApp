b376d45e6773d310ba931f863b5bfefdb6de23bc
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-02 10:29:57 +0530

    Add an @Update annotation with an update() function for one item

--- app/src/main/java/com/example/inventory/data/ItemDao.kt
+++ app/src/main/java/com/example/inventory/data/ItemDao.kt
@@ -3,10 +3,14 @@ package com.example.inventory.data
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Update
 
 @Dao
 interface ItemDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(item: Item)
+
+    @Update
+    suspend fun update(item: Item)
 }
