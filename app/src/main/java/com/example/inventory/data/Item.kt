383b6c9417fdf6e3dfe297b01213aa5a659410a9
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-02 10:25:24 +0530

    To identify the id as the primary key, annotate the id property with @PrimaryKey. Set the parameter autoGenerate to true so that Room generates the ID for each entity.

--- app/src/main/java/com/example/inventory/data/Item.kt
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -1,9 +1,11 @@
 package com.example.inventory.data
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "item")
 data class Item(
+    @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val itemName: String,
     val itemPrice: Double,
